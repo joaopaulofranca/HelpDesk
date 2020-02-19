cfa514d4a019555869872fdf80e2935ad7c9774c
joaopaulofranca <joaopaulofrancasilva@outlook.com>
2020-02-18 23:09:50 -0300

    Protegendo paginas restritas

--- index.php
+++ index.php
@@ -1,49 +1,67 @@
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <title>App Help Desk</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<head>
+  <meta charset="utf-8" />
+  <title>App Help Desk</title>
 
-    <style>
-      .card-login {
-        padding: 30px 0 0 0;
-        width: 350px;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-  <body>
+  <style>
+    .card-login {
+      padding: 30px 0 0 0;
+      width: 350px;
+      margin: 0 auto;
+    }
+  </style>
+</head>
 
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-         Help Desk
-      </a>
-    </nav>
+<body background="nature.jpg"style="background-repeat: no-repeat;background-size: cover;">
 
-    <div class="container">    
-      <div class="row">
+  <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+      Help Desk
+    </a>
+  </nav>
 
-        <div class="card-login">
-          <div class="card">
-            <div class="card-header">
-              Login - Help Desk
-            </div>
-            <div class="card-body">
-              <form>
-                <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail">
+  <div class="container">
+    <div class="row">
+
+      <div class="card-login">
+        <div class="card">
+          <div class="card-header">
+            Login
+          </div>
+          <div class="card-body">
+            <form action="valida_login.php" method="post">
+              <div class="form-group">
+                <input name="email" type="email" class="form-control" placeholder="E-mail">
+              </div>
+              <div class="form-group">
+                <input name="senha" type="password" class="form-control" placeholder="Senha">
+              </div>
+              <?php
+              if (isset($_GET['login']) && $_GET['login'] == 'erro') {
+              ?>
+                <div class="text-danger">
+                  Usuário ou senha inválido!
                 </div>
-                <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Senha">
+
+              <?php } ?>
+              <?php
+              if (isset($_GET['login']) && $_GET['login'] == 'erro2') {
+              ?>
+                <div class="text-danger">
+                  Faça o login antes de acessar as paginas protegidas!
                 </div>
-                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
-              </form>
-            </div>
+
+              <?php } ?>
+              <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+            </form>
           </div>
         </div>
+      </div>
     </div>
-  </body>
+</body>
+
 </html>
---
+++ valida_login.php
@@ -0,0 +1,29 @@
+<?php
+// protegendo paginas
+session_start();
+
+//variavel que verifica se a autenticação foi realizada
+$usuario_autenticado = false;
+//usuarios do sistema
+$usuarios_app = array(
+    array('email' => 'joaopaulofranca211@gmail.com', 'senha'=>'123456'),
+    array('email' => 'joaopaulofrancasilva@outlook.com', 'senha'=>'cont0596'),
+);
+
+foreach($usuarios_app as $user){
+    //echo 'Usuário app: ' . $user['email'] . '/' . $user['senha'];
+    //echo'<br/>';
+    //echo 'Usuário form: ' . $_POST['email'] . '/' . $_POST['senha'];
+   
+    if($user['email'] == $_POST['email'] && $user['senha'] ==  $_POST['senha'] ){
+        $usuario_autenticado = true;
+    }    
+}
+    if($usuario_autenticado){
+        echo'Usuário autenticado com sucesso!';
+        $_SESSION['autenticado'] = 'SIM';
+    }else{
+        $_SESSION['autenticado'] = 'NAO';
+        header('Location: index.php?login=erro');
+    }
+
